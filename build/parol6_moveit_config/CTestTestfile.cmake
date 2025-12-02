fb76a753560aaaf443de3e6977db7945c1f8e31a
Abdulkareem771 <wowkareem@gmail.com>
2025-12-02 15:06:56 +0300

    Fix: Add .gitignore and stop tracking build artifacts

--- build/parol6_moveit_config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/parol6_moveit_config
-# Build directory: /workspace/build/parol6_moveit_config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/parol6_moveit_config/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/workspace/install/parol6/share/parol6/package.sh"
