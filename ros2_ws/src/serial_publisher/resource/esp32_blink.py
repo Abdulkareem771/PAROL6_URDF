20835209cecbf71020143fe6e845feb1a08a0e9b
Abdulkareem771 <wowkareem@gmail.com>
2025-12-06 14:07:08 +0300

    feat: Add MicroPython script to blink an ESP32 LED.

---
+++ ros2_ws/src/serial_publisher/resource/esp32_blink.py
@@ -0,0 +1,15 @@
+from machine import Pin
+from time import sleep
+
+# On many ESP32 boards, the onboard LED is on GPIO 2.
+led = Pin(2, Pin.OUT)
+
+while True:
+    led.value(1)        # LED ON
+    print("LED ON")
+    sleep(1)
+    
+    led.value(0)        # LED OFF
+    print("LED OFF")
+    sleep(1)
+
