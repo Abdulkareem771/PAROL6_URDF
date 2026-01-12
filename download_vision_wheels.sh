1618b6227479cb2414e37b921ad55646b05cddd6
Osama Ahmed <77887osama@gmai.com>
2026-01-13 00:25:16 +0300

    fix: Update .gitignore to include wheel download directories and improve download script for PyTorch packages

--- download_vision_wheels.sh
+++ download_vision_wheels.sh
@@ -11,6 +11,7 @@ echo "1) CPU"
 echo "2) GPU (CUDA 12.6)"
 echo "3) GPU (CUDA 12.8)"
 echo "4) GPU (CUDA 13.0)"
+echo "To exit, Enter any other key."
 read -p "Enter choice: " choice
 
 echo "=========================================="
@@ -31,33 +32,32 @@ COMMON_FLAGS="\
 # Step 1: Download PyTorch FIRST (fixed versions)
 # -------------------------------------------------
 
-TORCH_PKGS="torch==2.9.0 torchvision==0.24.0 torchaudio==2.9.0"
+TORCH_PKGS="torch==2.9.1 torchvision==0.24.1 torchaudio==2.9.1"
 
 if [ "$choice" == "1" ]; then
     echo "Downloading CPU-only PyTorch..."
-    pip3 download -d "$WHEEL_DIR" $COMMON_FLAGS \
-        --index-url https://download.pytorch.org/whl/cpu \
-        $TORCH_PKGS
+    pip3 download -d "$WHEEL_DIR" "$COMMON_FLAGS" \
+        $TORCH_PKGS --index-url https://download.pytorch.org/whl/cpu
+        
 
 elif [ "$choice" == "2" ]; then
     echo "Downloading GPU PyTorch (CUDA 12.6)..."
-    pip3 download -d "$WHEEL_DIR" $COMMON_FLAGS \
-        --index-url https://download.pytorch.org/whl/cu126 \
-        $TORCH_PKGS
+    pip3 download -d "$WHEEL_DIR" "$COMMON_FLAGS" \
+        $TORCH_PKGS --index-url https://download.pytorch.org/whl/cu126
+    
 
 elif [ "$choice" == "3" ]; then
     echo "Downloading GPU PyTorch (CUDA 12.8)..."
-    pip3 download -d "$WHEEL_DIR" $COMMON_FLAGS \
-        --index-url https://download.pytorch.org/whl/cu128 \
-        $TORCH_PKGS
+    pip3 download -d "$WHEEL_DIR" "$COMMON_FLAGS" \
+        $TORCH_PKGS --index-url https://download.pytorch.org/whl/cu128
 
 elif [ "$choice" == "4" ]; then
     echo "Downloading GPU PyTorch (CUDA 13.0)..."
-    pip3 download -d "$WHEEL_DIR" $COMMON_FLAGS \
-        --index-url https://download.pytorch.org/whl/cu130 \
-        $TORCH_PKGS
+    pip3 download -d "$WHEEL_DIR" "$COMMON_FLAGS" \
+        $TORCH_PKGS --index-url https://download.pytorch.org/whl/cu130
+
 else
-    echo "Invalid choice"
+    echo "Eixting..."
     exit 1
 fi
 
@@ -70,11 +70,11 @@ echo "✅ PyTorch downloaded."
 echo "Downloading YOLO and vision libraries..."
 
 pip3 download -d "$WHEEL_DIR" $COMMON_FLAGS \
-    ultralytics \
+    pyyaml \
     opencv-python \
     scipy \
-    pyyaml
-
+    ultralytics
+    
 echo
 echo "✅ All packages downloaded into ./$WHEEL_DIR"
 echo "Directory size:"
