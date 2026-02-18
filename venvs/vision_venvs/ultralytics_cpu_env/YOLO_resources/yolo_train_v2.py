6d521aef34df87336fbe078432d44c9d0300d0eb
Osama Ahmed Radman <77887osama@gmail.com>
2026-02-18 06:22:14 +0300

    feat: Add YOLOv8n model training artifacts for experiment_9_v2 and correct model name in `yolo_train_v2.py`.

--- venvs/vision_venvs/ultralytics_cpu_env/YOLO_resources/yolo_train_v2.py
+++ venvs/vision_venvs/ultralytics_cpu_env/YOLO_resources/yolo_train_v2.py
@@ -21,7 +21,7 @@ RESULTS_FOLDER = project_dir / "yolo_training"   # replace with your folder path
 
 
 # Load a model
-model = YOLO("yolo8n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Train the model with 2 GPUs
 results = model.train(
