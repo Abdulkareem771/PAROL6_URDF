d7f4a9f284c35b008b654df597eeaa56086589c3
Osama Ahmed Radman <77887osama@gmail.com>
2026-02-21 01:41:27 +0300

    feat: Introduce path constants for model, image folders, and single image, then update example functions to utilize these new constants.

--- venvs/vision_venvs/ultralytics_cpu_env/YOLO_resources/example_crop_usage.py
+++ venvs/vision_venvs/ultralytics_cpu_env/YOLO_resources/example_crop_usage.py
@@ -12,9 +12,22 @@ current_dir = Path(__file__).parent
 project_dir = current_dir.parent
 
 
-print(f"current_dir={current_dir}")
-print(f"project_dir={project_dir}")
+#print(f"current_dir={current_dir}")
+#print(f"project_dir={project_dir}")
 
+
+# Path to your trained model
+MODEL_PATH = project_dir / "yolo_training" / "experiment_9" / "weights" / "best.pt"   # replace with your path
+
+# Path to the folder containing images to label
+IMAGE_FOLDER = project_dir / "data" / "Images_to_test"    # replace with your folder path
+
+# Path to the folder containing the cropped images "ROI"
+RESULTS_FOLDER = project_dir / "data" / "ROI_images"
+#RESULTS_FOLDER = project_dir / "data" / "ROI_images"
+
+# Path to single image
+SINGLE_IMAGE_PATH = project_dir / "data" / "Images_to_test" / "20260219_224015.jpg"
 
 # ==================== QUICK START EXAMPLES ====================
 
@@ -22,8 +35,8 @@ print(f"project_dir={project_dir}")
 def example_single_image():
     """Process a single image and crop detected objects."""
     process_single_image(
-        image_path= project_dir / "data" / "raw_model2_data" / "1.png",  # Change this to your image path
-        output_folder= project_dir / "data" / "ROI_images"
+        image_path= SINGLE_IMAGE_PATH,  # Change this to your image path
+        output_folder= RESULTS_FOLDER
     )
 
 
@@ -31,8 +44,8 @@ def example_single_image():
 def example_batch_processing():
     """Process all images in a folder."""
     process_image_folder(
-        input_folder="path/to/your/images",  # Change this to your images folder
-        output_folder="ROI_images"
+        input_folder=IMAGE_FOLDER,  # Change this to your images folder
+        output_folder=RESULTS_FOLDER
     )
 
 
