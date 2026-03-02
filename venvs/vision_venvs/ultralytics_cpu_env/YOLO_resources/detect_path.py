f1d570bafc5456a37a060e504fbbc28fcffbad20
Osama Ahmed Radman <77887osama@gmail.com>
2026-03-02 06:10:08 +0300

    chore: Update the single image path from `image_a4.png` to `image_a7.png`.

--- venvs/vision_venvs/ultralytics_cpu_env/YOLO_resources/detect_path.py
+++ venvs/vision_venvs/ultralytics_cpu_env/YOLO_resources/detect_path.py
@@ -17,7 +17,7 @@ CEXPAND_PX    = 10  # pixels to dilate each contour mask outward
 current_dir = Path(__file__)
 project_dir = current_dir.parent.parent
 
-SINGLE_IMAGE = project_dir / "data" / "some_images" / "image_a4.png"
+SINGLE_IMAGE = project_dir / "data" / "some_images" / "image_a7.png"
 
 IMAGE_FOLDER = project_dir / "data" / "Segmentation_images"
 
