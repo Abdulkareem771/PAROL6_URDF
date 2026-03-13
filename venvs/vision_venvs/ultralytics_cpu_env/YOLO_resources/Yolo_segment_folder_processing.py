58dab60c4046e2f0c2f30d0cd4145705afa5871f
Osama Ahmed Radman <77887osama@gmail.com>
2026-03-13 04:28:51 +0300

    feat: enable drawing of expanded object and intersection contours.

--- venvs/vision_venvs/ultralytics_cpu_env/YOLO_resources/Yolo_segment_folder_processing.py
+++ venvs/vision_venvs/ultralytics_cpu_env/YOLO_resources/Yolo_segment_folder_processing.py
@@ -117,22 +117,22 @@ def process_image(image_path):
     contour_obj1_exp = find_contours(obj_1_exp)
     contour_obj2_exp = find_contours(obj_2_exp)
     
-    """
+    
     if contour_obj1_exp is not None:
         cv2.drawContours(annotated, [contour_obj1_exp], -1, (0, 0, 255), 4)
 
     if contour_obj2_exp is not None:
         cv2.drawContours(annotated, [contour_obj2_exp], -1, (0, 0, 255), 4)
-    """
+    
 
     #intersection_mask = cv2.bitwise_and(obj_1, obj_2)   # Intersection mask without expand
     intersection_mask = cv2.bitwise_and(obj_1_exp, obj_2_exp)  # Intersection mask with expand
     contour_I = find_contours(intersection_mask)
-    
+    """
     if contour_I is not None:
         cv2.drawContours(annotated, [contour_I], -1, (0,0,255), -1)
         #cv2.drawContours(annotated, [contour_I], -1, (255,255,0), 3)
-    
+    """
     return annotated
 
 
