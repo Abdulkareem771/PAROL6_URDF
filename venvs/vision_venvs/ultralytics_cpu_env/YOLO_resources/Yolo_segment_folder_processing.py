2a6a5108355a965585b8595ca22451bfbabd7b71
Osama Ahmed Radman <77887osama@gmail.com>
2026-03-16 23:42:00 +0300

    feat: Update `DATA_DIR` to `Full_pipeline_vision` and enable drawing of object and intersection contours.

--- venvs/vision_venvs/ultralytics_cpu_env/YOLO_resources/Yolo_segment_folder_processing.py
+++ venvs/vision_venvs/ultralytics_cpu_env/YOLO_resources/Yolo_segment_folder_processing.py
@@ -19,8 +19,8 @@ CEXPAND_PX = 8
 # Paths
 # -----------------------------
 PROJECT_DIR = Path(__file__).parent.parent
-#DATA_DIR = PROJECT_DIR / "data" / "presentation_samples"
-DATA_DIR = PROJECT_DIR / "data" / "raw_images_for_models"
+DATA_DIR = PROJECT_DIR / "data" / "Full_pipeline_vision"
+#DATA_DIR = PROJECT_DIR / "data" / "raw_images_for_models"
 #SINGLE_IMAGE_PATH = PROJECT_DIR / "data" / "raw_images_for_models" / "image_13.jpg"
 OUTPUT_DIR = PROJECT_DIR / "data" / "Presentation_Samples_Results"
 
@@ -100,13 +100,13 @@ def process_image(image_path):
     contour_obj1 = find_contours(obj_1)
     contour_obj2 = find_contours(obj_2)
     
-    """
+    
     if contour_obj1 is not None:
         cv2.drawContours(annotated, [contour_obj1], -1, (0,0,255), 4)
 
     if contour_obj2 is not None:
         cv2.drawContours(annotated, [contour_obj2], -1, (0,0,255), 4)
-    """
+    
     
     dil_kernel = cv2.getStructuringElement(
         cv2.MORPH_ELLIPSE,
@@ -130,11 +130,11 @@ def process_image(image_path):
     #intersection_mask = cv2.bitwise_and(obj_1, obj_2)   # Intersection mask without expand
     intersection_mask = cv2.bitwise_and(obj_1_exp, obj_2_exp)  # Intersection mask with expand
     contour_I = find_contours(intersection_mask)
-    """
+    
     if contour_I is not None:
         cv2.drawContours(annotated, [contour_I], -1, (0,0,255), -1)
         #cv2.drawContours(annotated, [contour_I], -1, (255,255,0), 3)
-    """
+    
     return annotated
 
 
